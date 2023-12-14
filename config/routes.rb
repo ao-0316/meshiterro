5dd053c9ccf18f6d9a12e76db2372f4ae41a7ac9
ao-0316 <aosho0316.yukiusagi@gmail.com>
2023-12-14 08:20:30 +0000

    [Finish]4-22

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ get '/homes/about' => 'homes#about'
   
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 
